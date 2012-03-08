48ce8d46e8af588aad281032e234e235ccc0c098
Evgeny Zhulenev <evgeny.zhulenev@gmail.com>
2012-03-08 11:31:37 +0400

    Move ZMQ to another project

--- marketdb-core/src/main/scala/com/ergodicity/marketdb/stream/Heartbeat.scala
+++
@@ -1,9 +0,0 @@
-package com.ergodicity.marketdb.stream
-
-import java.util.UUID
-
-abstract sealed class Heartbeat {
-  def uid: UUID
-}
-case class Ping(uid: UUID) extends Heartbeat
-case class Pong(uid: UUID) extends Heartbeat
