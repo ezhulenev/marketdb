12fb992748ecd91da8e9596211d050d4d304af03
Evgeny Zhulenev <evgeny.zhulenev@gmail.com>
2012-08-19 18:46:29 +0400

    Bump up to scala 2.9.2

--- project/MarketDBBuild.scala
+++ project/MarketDBBuild.scala
@@ -9,7 +9,7 @@ object MarketDBBuild extends Build {
   lazy val buildSettings = Seq(
     organization := "com.ergodicity.marketdb",
     version      := "0.1-SNAPSHOT",
-    scalaVersion := "2.9.1"
+    scalaVersion := "2.9.2"
   )
 
   lazy val marketdb = Project(
@@ -58,6 +58,7 @@ object MarketDBBuild extends Build {
 
   lazy val repositoriesSetting = Seq(
     resolvers += "Sonatype Repository" at "http://oss.sonatype.org/content/groups/public/",
+    resolvers += "Scala Tools Repository" at "https://oss.sonatype.org/content/groups/scala-tools/",
     resolvers += "JBoss repository" at "http://repository.jboss.org/nexus/content/repositories/",
     resolvers += "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/",
     resolvers += "Typesafe Repository ide-2.9" at "http://repo.typesafe.com/typesafe/simple/ide-2.9/",
@@ -119,12 +120,12 @@ object Dependencies {
   val api = Seq(finagleCore, sbinary, jodaTime, jodaConvert, slf4jApi, logback, Test.scalatest, scalaTime)
 
   val core = Seq(ostrich, scalaTime, sbinary, finagleCore, finagleKestrel, scalaSTM, slf4jApi, logback, scalaz, cglib, jodaTime, jodaConvert) ++
-    Seq(Test.junit, Test.mockito, Test.powermockApi, Test.powermockJUnit, Test.scalatest, Test.scalacheck, Test.junitInterface) ++
+    Seq(Test.junit, Test.mockito, Test.powermockApi, Test.powermockJUnit, Test.scalatest, Test.junitInterface) ++
     Seq(asyncHBase, stumbleuponAsync, zookeeper)
 
 
   val loader = Seq(ostrich, finagleCore, finagleKestrel, scalaIO, httpClient, scalaTime, sbinary, jodaTime, jodaConvert, slf4jApi, logback, scalaz) ++
-    Seq(Test.scalatest, Test.scalacheck, Test.mockito)
+    Seq(Test.scalatest, Test.mockito)
 }
 
 
@@ -133,26 +134,25 @@ object Dependency {
   // Versions
 
   object V {
-    val Scalatest    = "1.6.1"
+    val Scalatest    = "1.8"
     val Slf4j        = "1.6.4"
     val Junit        = "4.5"
     val Mockito      = "1.8.1"
     val Powermock    = "1.4.11"
-    val Scalacheck   = "1.9"
     val Scalaz       = "6.0.4"
     val Cglib        = "2.2.2"
     val Logback      = "1.0.0"
-    val ScalaSTM     = "0.4"
+    val ScalaSTM     = "0.6"
     val JodaTime     = "2.0"
     val JodaConvert  = "1.2"
     val SBinary      = "0.4.0"
     val ScalaTime    = "0.5"
     val HttpClient   = "3.1"
-    val ScalaIO      = "0.3.0"
+    val ScalaIO      = "0.4.1"
 
     // Twitter dependencies
-    val Finagle      = "4.0.2"
-    val Ostrich      = "7.0.0"
+    val Finagle      = "5.3.6"
+    val Ostrich      = "8.2.3"
 
     // Async HBase transisitive hack
     val AsyncHBase              = "1.3.0"
@@ -169,9 +169,9 @@ object Dependency {
   val scalaSTM          = "org.scala-tools"                  %% "scala-stm"              % V.ScalaSTM
   val jodaTime          = "joda-time"                         % "joda-time"              % V.JodaTime
   val jodaConvert       = "org.joda"                          % "joda-convert"           % V.JodaConvert
-  val finagleCore       = "com.twitter"                      %% "finagle-core"           % V.Finagle
-  val finagleKestrel    = "com.twitter"                      %% "finagle-kestrel"        % V.Finagle
-  val ostrich           = "com.twitter"                      %% "ostrich"                % V.Ostrich
+  val finagleCore       = "com.twitter"                       % "finagle-core"           % V.Finagle
+  val finagleKestrel    = "com.twitter"                       % "finagle-kestrel"        % V.Finagle
+  val ostrich           = "com.twitter"                       % "ostrich"                % V.Ostrich
   val sbinary           = "org.scala-tools.sbinary"          %% "sbinary"                % V.SBinary
   val scalaTime         = "org.scala-tools.time"             %% "time"                   % V.ScalaTime intransitive()
   val httpClient        = "commons-httpclient"                % "commons-httpclient"     % V.HttpClient
@@ -201,7 +201,6 @@ object Dependency {
     val powermockApi   = "org.powermock"               % "powermock-api-mockito"   % V.Powermock    % "test"
     val powermockJUnit = "org.powermock"               % "powermock-module-junit4" % V.Powermock    % "test"
     val scalatest      = "org.scalatest"              %% "scalatest"               % V.Scalatest    % "it,test" // ApacheV2
-    val scalacheck     = "org.scala-tools.testing"    %% "scalacheck"              % V.Scalacheck   % "test" // New BSD
     val junitInterface = "com.novocode"                % "junit-interface"         % "0.8"          % "test"
   }
 }
